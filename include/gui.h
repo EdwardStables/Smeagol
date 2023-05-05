bd3bbcb988d8e94e9f08f3a0beeca316ce9b186f
EdwardStables <edward.stables1198@gmail.com>
2023-05-05 23:12:26 +0100

    canvas object and hovering

--- include/gui.h
+++ include/gui.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <optional>
+
 #include "olcPixelGameEngine.h"
 #include "state_manager.h"
 #include "state.h"
@@ -9,8 +11,10 @@ struct GUIState {
     olc::vf2d pos;
     float radius;
     StateBase state;
+    enum {NONE, HOVER, SELECT} draw_state = NONE;
 
     void draw(olc::PixelGameEngine& pge);    
+    bool intersects(olc::vf2d test_pos);
 
     GUIState(olc::vf2d pos, float radius, StateBase state) :
         pos(pos),
@@ -19,15 +23,38 @@ struct GUIState {
     {}
 };
 
+class StateCanvas {
+    olc::PixelGameEngine& pge;
+    StateManager& sm;
+    std::unordered_map<StateID, GUIState>& states;
+    const olc::vi2d _pos;
+    const olc::vi2d _size;
+public:
+    StateCanvas(
+        olc::PixelGameEngine& pge,
+        StateManager& sm,
+        olc::vi2d pos,
+        olc::vi2d size,
+        std::unordered_map<StateID, GUIState>& states
+    ) : pge(pge), sm(sm), _pos(pos), _size(size), states(states)
+    {}
+    void update();
+    void draw();
+    olc::vf2d size() {return _size;}
+    olc::vf2d pos() {return _pos;}
+    std::optional<olc::vf2d> screen_to_canvas(olc::vf2d screen_pos);
+};
+
 class SmeagolGUI : public olc::PixelGameEngine {
     StateManager& sm;
-
+    StateCanvas sc;
     std::unordered_map<StateID, GUIState> states;
-    olc::vi2d state_zone_start = {100, 0};
-    olc::vi2d state_zone_size = {699, 399};
 
 public:
-    SmeagolGUI(StateManager& sm) : sm(sm) {
+    SmeagolGUI(StateManager& sm)
+    : sm(sm),
+      sc(StateCanvas(*this, sm, {100,0}, {699, 399}, states))
+    {
         sAppName = "Smeagol";
     }
 public:
--- src/gui.cpp
+++ src/gui.cpp
@@ -2,8 +2,59 @@
 #include "layout.h"
 
 void GUIState::draw(olc::PixelGameEngine& pge){
-    pge.DrawCircle(pos, radius);
+    olc::Pixel colour;
+    switch (draw_state){
+        case HOVER: colour = olc::YELLOW; break;
+        case SELECT: colour = olc::DARK_YELLOW; break;
+        default: colour = olc::WHITE; break;
+    }
+    std::cout << state.id << " " << draw_state << std::endl;
+    pge.DrawCircle(pos, radius, colour);
     pge.DrawString(pos, state.name);
+}
+
+bool GUIState::intersects(olc::vf2d test_pos){
+    bool res = (test_pos - pos).mag() < radius;
+    return res;
+}
+
+std::optional<olc::vf2d> StateCanvas::screen_to_canvas(olc::vf2d screen_pos) {
+    olc::vf2d offset = screen_pos - _pos;
+
+    if (offset.x < 0.0f || offset.y < 0.0f) return std::nullopt;
+    if (offset.x > _size.x || offset.y > _size.y) return std::nullopt;
+
+    return offset;
+}
+
+void StateCanvas::update() {
+    // Update    
+    if (auto mpos = screen_to_canvas(pge.GetMousePos())){
+        for (auto &[id, state] : states){
+            if (state.intersects(mpos.value())){
+                state.draw_state = GUIState::HOVER;
+            } else {
+                state.draw_state = GUIState::NONE;
+            }
+        }
+    }
+}
+
+void StateCanvas::draw() {
+    olc::Sprite state_canvas(_size.x, _size.y);
+    pge.SetDrawTarget(&state_canvas);
+    for (auto &[id, state] : states){
+        state.draw(pge);
+
+        for (const auto &[input_id, target_ids] : sm.transitions.at(id)){
+            for (const auto &target_id: target_ids){
+                pge.DrawLine(state.pos, states.at(target_id).pos);
+            }
+        }
+    }
+    pge.SetDrawTarget(nullptr);
+    pge.DrawSprite(_pos, &state_canvas);
+    pge.DrawRect(_pos, _size);
 }
 
 bool SmeagolGUI::OnUserCreate() {
@@ -11,7 +62,7 @@ bool SmeagolGUI::OnUserCreate() {
         states.insert({id, GUIState({0,0}, 20, state)});
     }
 
-    Layout layout(sm, 3.0f*states.begin()->second.radius, state_zone_size.x, state_zone_size.y);
+    Layout layout(sm, 3.0f*states.begin()->second.radius, sc.size().x, sc.size().y);
 
     for (auto &[id, state] : states){
         state.pos = layout.pos(id);
@@ -23,23 +74,10 @@ bool SmeagolGUI::OnUserCreate() {
 
 // Called by olcConsoleGameEngine
 bool SmeagolGUI::OnUserUpdate(float fElapsedTime) {
-    // Clear Screen
+    // Drawing
     Clear(olc::BLACK);
-
-    olc::Sprite state_canvas(state_zone_size.x, state_zone_size.y);
-    SetDrawTarget(&state_canvas);
-    for (auto &[id, state] : states){
-        state.draw(*this);
-
-        for (const auto &[input_id, target_ids] : sm.transitions.at(id)){
-            for (const auto &target_id: target_ids){
-                DrawLine(state.pos, states.at(target_id).pos);
-            }
-        }
-    }
-    SetDrawTarget(nullptr);
-    DrawSprite(state_zone_start, &state_canvas);
-    DrawRect(state_zone_start, state_zone_size);
+    sc.update();
+    sc.draw();
 
     return true;
 }
